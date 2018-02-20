79cdf513ee0fa6605b3fdd56a0e8b550cdf3d961
Antonio Cucciniello <acucciniello@Antonios-MacBook-Pro.local>
2018-02-20 16:08:19 -0500

    Reviews Intent Handler

--- intents/reviews-intent.js
+++ intents/reviews-intent.js
@@ -1,9 +1,13 @@
 module.exports = ReviewsIntent
 
+var reviews = require('../yelp-fusion/rest-call.js').restCallForReview
+
 function ReviewsIntent () {
   var me = this
   var ids = this.attributes['restuarantIds']
-  reviews(ids, function(err, res) {
+  var numberOfId = this.event.request.intent.slots.idNumber.value
+  var id = ids[numberOfId]
+  reviews(id, function(err, res) {
     if (err) {
       console.log(err)
       var noReviews = 'I was unable to get reviews at this moment! Try again later'
